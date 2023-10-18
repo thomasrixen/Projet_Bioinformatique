32f168b7a8d9f37211fcb154e61b9fcb071254b3
Thomas Rixen <thomas.rixen@student.uclouvain.be>
2023-10-18 12:26:23 +0200

    commentaires

--- A1/A1.5.R
+++ A1/A1.5.R
@@ -9,7 +9,16 @@ sequence_length <- length(sequence)
 start_codons <- c("ttg", "ctg", "ata", "att", "atc", "atg", "gtg")
 stop_codons <- c("tga", "taa", "tag")
 
-find_frame_start <- function(sequence, frame_size, increment, target_string) {
+#Argument:
+# - sequence: genome input
+# - frame_size: taille du codon (mit à 3 par défaut)
+# - increment: Permet de bouger la frame (on utilisera 1 pour trouver tout les start et stop et 3 une fois qu'on veut trouver le premier stop qui suit un start)
+# - target_string: correspond à une string de taille 3, des codons start ou stop
+
+#Return
+#Retourne une list avec tout les indexes de la sequence où commence un codon (target_string)
+
+find_frame_start <- function(sequence, frame_size = 3, increment, target_string) {
     indices <- c() 
     
     target_length <- nchar(target_string)
@@ -33,14 +42,23 @@ find_frame_start <- function(sequence, frame_size, increment, target_string) {
   return(indices)
 }
 
+#Exemple d'utilisation
 # test_sequence <- c("t", "t", "c", "t", "g", "t", "a", "c", "t", "g")
 # target_string <- "ctg"
 # frame_size <- 3
 # increment <- 1
 
 # result <- find_frame_start(test_sequence, frame_size, increment, target_string)
-# print(result)
+# print(result) #return 3 et 8
 
+#
+
+#Argument:
+# - sequence: genome input
+# - codons: list de codons dont on veut trouver les indexes
+
+#Return
+#Une liste triée avec tout les indexes de codons start ou stop
 
 get_codons_index <- function(sequence, codons){
     list <- c()
@@ -51,10 +69,7 @@ get_codons_index <- function(sequence, codons){
     return(sort_list)
 }
 
-
-# frame_size <- 3
-# increment <- 1
-# target_string <- "ctg"
+#Exemple d'utilisation
 
 # start_index <- find_frame_start(genome_sequence, frame_size, increment, target_string)
 # cat("Indexes of the start of the frame:", start_index, "\n")
