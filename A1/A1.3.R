56ad0b42df13ed7dd4d83ce736da3fe96faa55c7
Thomas Rixen <thomas.rixen@student.uclouvain.be>
2023-10-22 17:16:01 +0200

    comment A1.5 and correction A1.3

--- A1/A1.3.R
+++ A1/A1.3.R
@@ -1,4 +1,5 @@
 library(ape)
+
 #=================================================#
 #                   QUESTION 1                    #
 #=================================================#
@@ -32,25 +33,39 @@ saveRDS(dimer_matrix, "dimer_frequencies.rds")
 #=================================================#
 
 
-seq <- paste(seq, collapse = "")
 
-# Create the observed_dimer_frequencies matrix
 observed_dimer_frequencies <- matrix(0, nrow = 4, ncol = 4)
 rownames(observed_dimer_frequencies) <- c("A*", "C*", "G*", "T*")
 colnames(observed_dimer_frequencies) <- c("*A", "*C", "*G", "*T")
 
-# Function to count dimers in a DNA sequence
-count_dimer <- function(nuc1, nuc2, seq) {
-  dimers <- paste0(nuc1, nuc2)
-  count <- nchar(gsub(paste0("(?=", dimers, ")"), "", seq, perl = TRUE)) / 2
-  return(count)
+
+count_dimer <- function(nuc1, nuc2, seq){
+    index <- 1
+    count <- 0
+    len_seq <- length(seq)
+
+    while(index < len_seq){
+        if (index + 1 > len_seq) {
+           break
+        }
+        if (!is.na(seq[index]) && !is.na(seq[index + 1]) && seq[index] == nuc1 && seq[index + 1] == nuc2) {
+            count <- count + 1
+        }
+        index <- index + 1
+
+    }
+    count
 }
 
+
 for (colonne in colnames(observed_dimer_frequencies)) {
-  for (ligne in rownames(observed_dimer_frequencies)) {
-    observed_dimer_frequencies[ligne, colonne] = count_dimer(get_lower_letter(ligne), get_lower_letter(colonne), seq)
-  }
+    for (ligne in rownames(observed_dimer_frequencies)) {
+      observed_dimer_frequencies[ligne, colonne] = count_dimer(get_lower_letter(ligne), get_lower_letter(colonne), seq)/sequence_length
+      #print(observed_dimer_frequencies)
+    }
 }
+print(observed_dimer_frequencies)
+
 
 get_lower_letter <- function(input_string) {
   if (grepl("[A-Z]\\*", input_string)) {
@@ -64,22 +79,19 @@ get_lower_letter <- function(input_string) {
   }
 }
 
-# Print the observed dimer frequencies
-print("This is the observed dimer frequencies in the sequence:")
-print(observed_dimer_frequencies)
+print("This is the observed dimer frequencies in the sequence :")
+observed_dimer_frequencies
 
-# Function to get the top labels
 get_top_labels <- function(matrix, get_largest = TRUE) {
-  values <- as.vector(matrix[, -1])
-  labels <- matrix[, 1]
-  
+  values <- as.vector(matrix[ , -1])
+  labels <- matrix[ , 1]
+
   sorted_indices <- ifelse(get_largest, order(values, decreasing = TRUE), order(values))
-  
+
   top_indices <- sorted_indices[1:3]
   top_labels <- labels[top_indices]
-  
+
   return(top_labels)
 }
 
-# Save the observed dimer frequencies to an RDS file
 saveRDS(observed_dimer_frequencies, "observed_dimer_frequencies.rds")
--- A1/A1.5.R
+++ A1/A1.5.R
@@ -1,113 +1,93 @@
+# Load the 'ape' library for working with DNA sequences
 library(ape)
 
+# Specify the filename for the DNA sequence in FASTA format
 sequence_filename <- "bacterial_sequence.fasta"
 
+# Read the DNA sequence from the file
 mat <- read.dna(sequence_filename, format = "fasta", as.character = TRUE)
 genome_sequence <- as.vector(mat)
-sequence_length <- length(sequence)
+sequence_length <- length(sequence)  # Should be 'genome_sequence' instead of 'sequence'
 
+# Define start and stop codons
 start_codons <- c("ttg", "ctg", "ata", "att", "atc", "atg", "gtg")
 stop_codons <- c("tga", "taa", "tag")
 
-#Argument:
-# - sequence: genome input
-# - frame_size: taille du codon (mit à 3 par défaut)
-# - increment: Permet de bouger la frame (on utilisera 1 pour trouver tout les start et stop et 3 une fois qu'on veut trouver le premier stop qui suit un start)
-# - target_string: correspond à une string de taille 3, des codons start ou stop
-
-#Return
-#Retourne une list avec tout les indexes de la sequence où commence un codon (target_string)
-
+# Define a function to find the start positions of a given codon in a DNA sequence
 find_frame_start <- function(sequence, frame_size = 3, increment, target_string) {
     indices <- c() 
-    
+
     target_length <- nchar(target_string)
     sequence_length <- length(sequence)
     
     for (i in 1:(sequence_length - frame_size + 1)) {
         frame <- paste(sequence[i:(i + frame_size - 1)], collapse = "")
-        
+
         if (frame == target_string) {
-        indices <- c(indices, i)
-        i <- i + target_length - 1
+            indices <- c(indices, i)
+            i <- i + target_length - 1
         } else {
-        i <- i + increment - 1
+            i <- i + increment - 1
         }
         
         if (i > sequence_length) {
-        break
+            break
         }
     }
-  
-  return(indices)
+
+    return(indices)
 }
 
-#Exemple d'utilisation
-
-test_sequence <- c("t", "t", "c", "t", "g", "t", "a", "c", "t", "g", "t", "t", "a", "g", "a", "t")
-# target_string <- "ctg"
-# frame_size <- 3
-# increment <- 1
-
-# result <- find_frame_start(test_sequence, frame_size, increment, target_string)
-# print(result) #return 3 et 8
-
-#
-
-#Argument:
-# - sequence: genome input
-# - codons: list de codons dont on veut trouver les indexes
-
-#Return
-#Une liste triée avec tout les indexes de codons start ou stop
-
-get_codons_index <- function(sequence, codons){
+# Define a function to find the indexes of multiple codons in a DNA sequence
+get_codons_index <- function(sequence, codons) {
     list <- c()
     for (codon in codons) {
-       list <- c(list, find_frame_start(sequence, 3, 1, codon))
+        list <- c(list, find_frame_start(sequence, 3, 1, codon))
     }
     sort_list <- sort(list)
     return(sort_list)
 }
 
-count_len <- function(start_indexes, stop_indexes, min_len, len_seq){
+# Define a function to count the length of open reading frames (ORFs)
+count_len <- function(start_indexes, stop_indexes, min_len, len_seq) {
     count <- 0
     start_index <- 1
-    while(start_index < length(start_indexes)){
+    while (start_index < length(start_indexes)) {
         index <- start_indexes[start_index] + 3
-        while(!is.element(index, stop_indexes) && index <= len_seq-2){
+        while (!is.element(index, stop_indexes) && index <= len_seq - 2) {
             index <- index + 3
         }
-        if ((index + 3)-start_indexes[start_index] >= min_len) {
-           count <- count + 1
-        # uncomment if you want to have details about all the sequence   
-        #    cat("The codon nr :", count, "\n")
-        #    cat("start_index = ", start_indexes[start_index], "\n")
-        #    cat("end_index = ", index, "\n")
-        #    cat("longueur = ", (index + 3)-start_indexes[start_index], "\n")
+        if ((index + 3) - start_indexes[start_index] >= min_len) {
+            count <- count + 1
         }
-        while(start_indexes[start_index] < index + 3 && start_index <= length(start_indexes)){
+        while (start_indexes[start_index] < index + 3 && start_index <= length(start_indexes)) {
             start_index <- start_index + 1
         }
     }
     return(count)
 }
 
+# Get the indexes of all start codons in the DNA sequence
 list_start <- get_codons_index(genome_sequence, start_codons)
 cat("Indexes of all the start codons:", list_start, "\n")
 
+# Get the indexes of all stop codons in the DNA sequence
 list_stop <- get_codons_index(genome_sequence, stop_codons)
 cat("Indexes of all the stop codons:", list_stop, "\n")
 
+# Initialize a vector to store the counts of ORFs of different lengths
 ORFs_result <- c(0, 0, 0, 0, 0, 0)
 labels <- c("0", "10", "50", "100", "300", "500")
-k_nuc = c(0, 10, 50, 100, 300, 500)
+k_nuc <- c(0, 10, 50, 100, 300, 500)
 names(ORFs_result) <- labels
 
+# Count the number of ORFs of different lengths
 for (k in k_nuc) {
-   ORFs_result[as.character(k)] <- count_len(list_start, list_stop, k, length(genome_sequence))
+    ORFs_result[as.character(k)] <- count_len(list_start, list_stop, k, length(genome_sequence))
 }
 
+# Display the results
 ORFs_result
 
+# Save the results as an RDS file
 saveRDS(ORFs_result, file = "ORFs_result.rds")
